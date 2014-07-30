e0a20307a88dc4cfdede58a4d47f224cc23b87cb
Amine Benseddik <amine.benseddik@gmail.com>
2014-07-30 12:10:04 +0200

    Adding sample files, minor syntax changes

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 csv2ical.js
 ===========
+
+Quick and dirty CSV to iCal convertor
+
+Usage: csv2ical.js -i [csv] -o [ics] -d [delimiter] -H
+
+Examples:
+  csv2ical.js -i query_semicolon.csv -o test.ics -d ";" -H
+
+
+Options:
+  -i            Input CSV file                                                              [required]
+  -o            Output ICS file                                                             [required]
+  -d            If your data uses an alternate delimiter such as ";"                        [default: ","]
+  -H            To set if the first line of your CSV contain headers                        [default: false]
+  --rows        Row numbers for "Subject","Start Date","End Date" ... : --rows={0,1,2,3,4}  [default: 0,1,2,3,4]
+  --dateformat  Date format 31/07/2014 03:00 : --dateformat="DD-MM-YYYY HH:mm"
+
+
+Example :
+./csv2ical.js -i sample.csv -o sample.ics -H
+Subject : Darth Vader birthday
+Start Date : 2014-07-30 18:00
+End Date : 2014-07-31 03:00
+Description : Super Big party
+Location : Death star
+-------------
+Subject : C3PO birthday
+Start Date : 2014-08-01 16:00
+End Date : 2014-08-01 18:00
+Description : Super Big party
+Location : Tatooine
+-------------
+Subject : R2D2 birthday
+Start Date : 2014-08-24 09:00
+End Date : 2014-08-24 23:00
+Description : Super Big party
+Location : Coruscant
+-------------
+Saving to file : sample.ics
--- csv2ical.js
+++ csv2ical.js
@@ -3,31 +3,31 @@
 // Usage : node csv2ical.js -i <input csv file> -o <output ics file> -d <delimiter char>
 
 var argv = require('yargs')
-  .usage('Usage: csv2ical.js -i [csv] -o [ics] -d [delimiter] -H')
-  .default('d', ",")
-  .default('H', false)
-  .default('rows', ["0", "1", "2", "3", "4"])
-  .demand(['i','o'])
-  .describe('i', 'Input CSV file')
-  .describe('o', 'Output ICS file')
-  .describe('d', 'If your data uses an alternate delimiter such as ";"')
-  .describe('H', 'To set if the first line of your CSV contain headers')
-  .describe('rows', 'Row numbers for "Subject","Start Date","End Date" ... : --rows={0,1,2,3}')
-  .describe('dateformat', 'Date format 31/07/2014 03:00 : --dateformat="DD-MM-YYYY HH:mm"')
-  .example('csv2ical.js -i query_semicolon.csv -o test.ics -d ";" -H', '')
-  .argv;
+	.usage('Usage: csv2ical.js -i [csv] -o [ics] -d [delimiter] -H')
+	.default('d', ",")
+	.default('H', false)
+	.default('rows', ["0", "1", "2", "3", "4"])
+	.demand(['i','o'])
+	.describe('i', 'Input CSV file')
+	.describe('o', 'Output ICS file')
+	.describe('d', 'If your data uses an alternate delimiter such as ";"')
+	.describe('H', 'To set if the first line of your CSV contain headers')
+	.describe('rows', 'Row numbers for "Subject","Start Date","End Date" ... : --rows={0,1,2,3,4}')
+	.describe('dateformat', 'Date format 31/07/2014 03:00 : --dateformat="DD-MM-YYYY HH:mm"')
+	.example('csv2ical.js -i sample.csv -o sample.ics -d ";" -H', '')
+	.argv;
 
 //console.log(argv);
 //console.log(argv.rows[2]);
 
 // Init iCal
 var ical = require('ical-generator'),
-    cal = ical();
+	cal = ical();
 
 cal.setProdID({
-    company: 'My Company',
-    product: 'My Product',
-    language: 'EN'
+	company: 'My Company',
+	product: 'My Product',
+	language: 'EN'
 });
 cal.setDomain('mycompany.com').setName('My Calendar');
 
@@ -40,23 +40,23 @@ var fs = require('fs');
 var stream = fs.createReadStream(argv.i);
 
 csv
- .fromStream(stream, {headers : argv.H, delimiter: argv.d})
- .on("record", function (data){
+.fromStream(stream, {headers : argv.H, delimiter: argv.d})
+.on("record", function (data){
 
-  // "Subject","Start Date","End Date","Description"
+	// "Subject","Start Date","End Date","Description"
 	//console.log(data);
-  console.log("Subject : "     + data[Object.keys(data)[argv.rows[0]]]); // Subject
-  console.log("Start Date : "  + data[Object.keys(data)[argv.rows[1]]]); // Start Date
+	console.log("Subject : "     + data[Object.keys(data)[argv.rows[0]]]); // Subject
+	console.log("Start Date : "  + data[Object.keys(data)[argv.rows[1]]]); // Start Date
 	console.log("End Date : "    + data[Object.keys(data)[argv.rows[2]]]); // End Date
 	console.log("Description : " + data[Object.keys(data)[argv.rows[3]]]); // Description
-  console.log("Location : "    + data[Object.keys(data)[argv.rows[4]]]); // Description
-  console.log("-------------");
+	console.log("Location : "    + data[Object.keys(data)[argv.rows[4]]]); // Location
+	console.log("-------------");
 
 	var startdate = moment(data[Object.keys(data)[argv.rows[1]]], argv.dateformat);
 	var enddate   = moment(data[Object.keys(data)[argv.rows[2]]], argv.dateformat) ;
 
-	console.log(startdate.isValid());
-	console.log(enddate.isValid());
+	//console.log(startdate.isValid());
+	//console.log(enddate.isValid());
 
 
 	if(startdate.isValid() && enddate.isValid())
@@ -71,9 +71,9 @@ csv
 		});
 	}
 
- })
- .on("end", function(){
-   console.log(" Saving to file : " + argv.o);
-	 cal.saveSync(argv.o);
+})
+.on("end", function(){
+	console.log("Saving to file : " + argv.o);
+	cal.saveSync(argv.o);
 
- });
+});
