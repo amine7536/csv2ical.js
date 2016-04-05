583a5f6fbd368e35bf40b54514e3c2991f74edf4
Amine Benseddik <amine.benseddik@pixelfactory.com>
2016-04-06 01:04:42 +0200

    adding build script

--- src/csv2ical.js
+++ src/csv2ical.js
@@ -23,7 +23,7 @@ const argv = require('yargs')
   .describe('H', 'To set if the first line of your CSV contain headers')
   .describe('rows', 'Row numbers for "Subject","Start Date","End Date" ... : --rows={0,1,2,3,4}')
   .describe('dateformat', 'Date format 31/07/2014 03:00 : --dateformat="DD-MM-YYYY HH:mm"')
-  .example('csv2ical.js -i sample.csv -o sample.ics', '-d ";" -H ')
+  .example('csv2ical -i sample.csv -o sample.ics', '-d "," -H ')
   .argv;
 
 /**
